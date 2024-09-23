f7c0e408ab8b63fc9f0e5ec8aaac4d711a615a60
Tade03 <tade.castillo.03@gmail.com>
2024-09-23 03:29:45 -0600

    Initial Commit

---
+++ src/test/kotlin/com/bakerwins/Backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bakerwins.Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
